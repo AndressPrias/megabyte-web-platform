a54144ef3d9a6daa4026ec1ac1279e5b49a2de92
Andrest <anpriasca@gmail.com>
2026-07-18 16:13:24 -0500

    Make active publish switch green

--- admin/index.php
+++ admin/index.php
@@ -15,7 +15,7 @@ mb_start_admin_session();
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Montserrat:wght@600;700&family=Poppins:wght@300;400&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="/css/styles.css?v=site-20260718-7">
+  <link rel="stylesheet" href="/css/styles.css?v=site-20260718-8">
   <style>
     .store-admin__login[hidden],
     .store-admin__login[data-rendered='true'][hidden],
